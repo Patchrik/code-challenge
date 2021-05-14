532746e03104199542035857ea8f51b85346ef61
Parker Kelley <Pklly92@gmail.com>
2021-05-14 08:02:17 -0500

    Fixing the api

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,61 +14,61 @@ import { useQuery } from "react-query";
 import { getSearchPerson } from "./api/api";
 
 const Home = () => {
-  const { light, setLight } = useTheme();
-  const [search, setSearch] = React.useState<string>("");
+	const { light, setLight } = useTheme();
+	const [search, setSearch] = React.useState<string>("");
 
-  //useQuery for character data
-  const { data }: any = useQuery(
-    ["characterSearch", { search }],
-    () => getSearchPerson(),
-    {
-      enabled: !search,
-    }
-  );
+	//useQuery for character data
+	const { data }: any = useQuery(
+		["characterSearch", { search }],
+		() => getSearchPerson(search),
+		{
+			enabled: !search,
+		}
+	);
 
-  // return if error
-  if (error) return "An error has occurred: " + error.message;
+	// return if error
+	if (error) return "An error has occurred: " + error.message;
 
-  return (
-    <div className={styles.imgContainer}>
-      <Grid direction="row" container style={{ paddingTop: "25px" }}>
-        {/* search bar and custom loading spinner */}
-        <Search light={light} setSearch={setSearch} status={status} />
-        {/* dark/light btn */}
-        <ModeBtn light={light} setLight={setLight} />
-      </Grid>
-      {data && data.length > 0 && (
-        <Grid
-          direction="row"
-          container
-          justify="flex-start"
-          alignContent="center"
-          style={{
-            paddingTop: "50px",
-            paddingLeft: "50px",
-            paddingBottom: "50px",
-          }}
-        >
-          <CharacterDetails light={light} data={data} />
-        </Grid>
-      )}
-      {/* no search results */}
-      {data && data.length === 0 && (
-        <Grid
-          container
-          direction="row"
-          xs={12}
-          style={{
-            color: light ? "blue" : "red",
-            paddingLeft: "50px",
-            fontSize: "18px",
-          }}
-        >
-          No character matched this search. Check spelling, and try again.
-        </Grid>
-      )}
-    </div>
-  );
+	return (
+		<div className={styles.imgContainer}>
+			<Grid direction="row" container style={{ paddingTop: "25px" }}>
+				{/* search bar and custom loading spinner */}
+				<Search light={light} setSearch={setSearch} status={status} />
+				{/* dark/light btn */}
+				<ModeBtn light={light} setLight={setLight} />
+			</Grid>
+			{data && data.length > 0 && (
+				<Grid
+					direction="row"
+					container
+					justify="flex-start"
+					alignContent="center"
+					style={{
+						paddingTop: "50px",
+						paddingLeft: "50px",
+						paddingBottom: "50px",
+					}}
+				>
+					<CharacterDetails light={light} data={data} />
+				</Grid>
+			)}
+			{/* no search results */}
+			{data && data.length === 0 && (
+				<Grid
+					container
+					direction="row"
+					xs={12}
+					style={{
+						color: light ? "blue" : "red",
+						paddingLeft: "50px",
+						fontSize: "18px",
+					}}
+				>
+					No character matched this search. Check spelling, and try again.
+				</Grid>
+			)}
+		</div>
+	);
 };
 
 export default Home;
